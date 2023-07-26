7d7ae8eec1db3180eb96b38caee74b756cfdd725
jparker1221 <jonparker1221@gmail.com>
2023-07-25 21:30:27 -0400

    added profile picture

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,11 +1,12 @@
 import React from 'react';
-// import ProfilePic from '';
+import ProfilePic from '../assets/images/headshot.jpg';
 
 function About() {
     return (
         <div>
             <h1 className='header'>About Me</h1>
             <section style={{ fontFamily: '' }} className='body'>
+                <img src={ProfilePic} alt="Jon Parker Profile" style={{ width: '500px', height: 'auto' }} />
                 <p>
                     Hi! I'm Jon Parker. I'm a full-stack web developer and a recent graduate of the UNC-Chapel Hill Coding Bootcamp. I also hold a BBA degree from NC State University and have several years of experience working in analytics and customer service for companies such as Ralph Lauren and Dewey's Bakery. I'm originally from Mooresville, NC and currently live in Winston-Salem, NC with my wife, Tara, and our Great Dane, Waylon.
                     <br></br>
